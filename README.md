c09f556844a0296656e1375ea91e348141a9234d
jalbertomr <jalbertomr@yahoo.com>
2023-02-26 17:16:06 -0600

    Readme

--- README.md
+++ README.md
@@ -1,30 +1,54 @@
 # Functional Interface #
 
-## Function ##
+## Function Input T class -> Output R Class ##
 
 Function
 
-## Predicate ##
+  Define the function <T,R> one Input, one output 
+  
+  Function.apply(T t)
+  
+  Function.andThen(...)
+  <V> Function<T,V> andThen(Function <? super R, ? extends V after)
+  
+  Function.compose(...)
+  <V> Function<V,R> compose(Function<? super V, ? extends T> before)
+  
+  Function.identity()
+  static <T> Function<T,T> identity()
+
+## Predicate Input T -> Output boolean ##
 
 Predicate
 
-## ByPredicate ##
+## BiPredicate Input T, Input U -> Output boolean ##
 
-ByPredicate
+BiPredicate
 
-## UnaryOperator ##
+## UnaryOperator Input T class -> T class ##
 
 UnaryOperator
 
-## BinaryOperator ##
+## BinaryOperator Input T class, T class -> T class ##
 
 BinaryOperator
 
-## Supplier ##
+## Supplier Input ---, Output T class ##
 
 Supplier
+   .get(): T
 
-## Consumer ##
+## Consumer Input T class, Output --- ##
 
 Consumer
+  .accept(T)
+  .andThen( Consumer<? super T>): Consumer<T>
+
+## BiConsumer Input T class, U class, Output --- ##
+
+BiConsumer
+  .accept(T)
+  .andThen(Consumer<? super T> Consumer<? super U>): BiConsumer<T,U>
+  
+
 
--- src/main/java/com/bext/FunctionTest.java
+++ src/main/java/com/bext/FunctionTest.java
@@ -1,32 +1,66 @@
 package com.bext;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
+/**
+ * Uso de Function
+ * 
+ * @author bext
+ *
+ */
 public class FunctionTest {
     public static void main(String... args){
-        Function<Integer, Integer> add3 = (t) -> t + 3;
-        Function<Integer, Integer> multiply2 = (t) -> t * 2;
-        Function<Integer, Integer> multiplyThenAdd = multiply2.compose( add3);
-
-        System.out.println( add3.apply(2)      );   //output: 5
-        System.out.println( multiply2.apply(4));  //output: 8
+    	/**
+    	 *  Definiciones de Functions
+    	 */
+        Function<Integer, Integer> functionAdd3 = (t) -> t + 3;
+        Function<Integer, Integer> functionMultiply2 = (t) -> t * 2;
+        /** 
+         * Function.compose to add more Functions to Function.
+         */
+        Function<Integer, Integer> functionMultiplyThenAdd = functionMultiply2.compose( functionAdd3);
+        /**
+         * Function.identity to return the input value. not necesary just call the static.
+         */
+        Function<Integer,Integer> functionIdentity = Function.identity();
+        
+        System.out.println( functionAdd3.apply(2)      );   //output: 5
+        System.out.println( functionMultiply2.apply(4));  //output: 8
         System.out.println(
-                add3.andThen( multiply2).apply( 3)  //output: 12
+                functionAdd3.andThen( functionMultiply2).apply( 3)  //output: 12
         );
         System.out.println(
-                add3.andThen( add3)
-                    .andThen( add3).apply( 1)    //output: 10
+                functionAdd3.andThen( functionAdd3)
+                    .andThen( functionAdd3).apply( 1)    //output: 10
         );
 
         System.out.println(
-                add3.andThen( add3)
-                        .andThen( add3)
-                        .andThen( multiply2)
+                functionAdd3.andThen( functionAdd3)
+                        .andThen( functionAdd3)
+                        .andThen( functionMultiply2)
                         .apply( 1)    //output: 20
         );
 
-        System.out.println( multiplyThenAdd.apply( 4));  // (4 + 3) * 2 = 14
+        System.out.println( functionMultiplyThenAdd.apply( 4));  // (4 + 3) * 2 = 14
 
+        System.out.println( functionIdentity.apply(111)); // 111
+        
+        List<String> names = Arrays.asList("Hugo", "Paco", "Luis", "Daisy");
+
+        names.stream()
+        .map(e -> e)
+        .forEach(System.out::println);
+        
+        names.stream()
+        .map(Function.identity())
+        .map(e -> e.concat(" "))
+        .forEach(System.out::println);;
+        
+        System.out.println();
+        
         Function<Integer, String> functionInt_String = t -> t * 2 + "_doubled.";
         Function<String, Integer> functionString_Int = t -> t.length();
 
